7059d0020853a27539f36d2a46c50aacc60e6460
Dylan Hallgren <Dylan.Hallgren@vu.com>
2025-01-07 13:04:15 -0600

    updated working pay calculator!

--- payCalculator.py
+++ payCalculator.py
@@ -2,13 +2,17 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = float(input("Enter Hours:"))
+    rate = float(input("Enter Rate"))
+    pay = float(hrs * rate)
+    print(pay)
 
     # end assignment
 
 
 ## If you want to test locally before you try to sync
 ## Open your terminal and run > python payCalculator.py
+
 
 #Ignore this for now. 
 if __name__ == "__main__":
